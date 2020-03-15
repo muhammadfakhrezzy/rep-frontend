2206523d53b735d26a3d84998440f5f605f2eea1
hibiru-code <hibiru.code@gmail.com>
2020-03-15 23:26:21 +0700

    feat: pharse content main to different file

---
+++ src/components/views/DashboardView.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Dashboard</h1>
+                    </div>
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card card-widget widget-user">
+                                <div class="widget-user-header" style="background-color: #BF814D">
+                                    <h3 class="widget-user-username">RANK 3</h3>
+                                    <h5 class="widget-user-desc">Angga Widianto</h5>
+                                </div>
+                                <div class="widget-user-image">
+                                    <img src="@/assets/img/user5-128x128.jpg" class="img-circle elevation-2">
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="description-block">
+                                            <h5 class="description-header">876</h5>
+                                            <span class="description-text">POIN</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card card-widget widget-user">
+                                <div class="widget-user-header" style="background-color: #E7AB30">
+                                    <h3 class="widget-user-username">RANK 1</h3>
+                                    <h5 class="widget-user-desc">Muhammad Iqbal</h5>
+                                </div>
+                                <div class="widget-user-image">
+                                    <img src="@/assets/img/user1-128x128.jpg" class="img-circle elevation-2">
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="description-block">
+                                        <h5 class="description-header">1091</h5>
+                                        <span class="description-text">POIN</span>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card card-widget widget-user">
+                                <div class="widget-user-header" style="background-color: #BFBFBF">
+                                    <h3 class="widget-user-username">RANK 2</h3>
+                                    <h5 class="widget-user-desc">Aditya Lityanian</h5>
+                                </div>
+                                <div class="widget-user-image">
+                                    <img src="@/assets/img/user4-128x128.jpg" class="img-circle elevation-2">
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="description-block">
+                                        <h5 class="description-header">931</h5>
+                                        <span class="description-text">POIN</span>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header border-0">
+                                    <h3 class="card-title">Top 10</h3>
+                                </div>
+                                <div class="card-body table-responsive p-0">
+                                    <table class="table table-striped table-valign-middle">
+                                        <thead>
+                                            <tr></tr>
+                                            <tr></tr>
+                                            <tr></tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>04</td>
+                                                <td>
+                                                    <img src="@/assets/img/default-150x150.png" class="img-circle img-size-32 mr-2">
+                                                    Amin Bahrul Ulum
+                                                </td>
+                                                <td>822 POIN</td>
+                                            </tr>
+                                            <tr>
+                                                <td>05</td>
+                                                <td>
+                                                    <img src="@/assets/img/default-150x150.png" class="img-circle img-size-32 mr-2">
+                                                    Fahri Tri Setio
+                                                </td>
+                                                <td>789 POIN</td>
+                                            </tr>
+                                            <tr>
+                                                <td>06</td>
+                                                <td>
+                                                    <img src="@/assets/img/default-150x150.png" class="img-circle img-size-32 mr-2">
+                                                    Muhammad Zamrozi
+                                                </td>
+                                                <td>751 POIN</td>
+                                            </tr>
+                                            <tr>
+                                                <td>07</td>
+                                                <td>
+                                                    <img src="@/assets/img/default-150x150.png" class="img-circle img-size-32 mr-2">
+                                                    Damar Pamungkas
+                                                </td>
+                                                <td>721 POIN</td>
+                                            </tr>
+                                            <tr>
+                                                <td>08</td>
+                                                <td>
+                                                    <img src="@/assets/img/default-150x150.png" class="img-circle img-size-32 mr-2">
+                                                    Miftahul Fallah
+                                                </td>
+                                                <td>579 POIN</td>
+                                            </tr>
+                                            <tr>
+                                                <td>09</td>
+                                                <td>
+                                                    <img src="@/assets/img/default-150x150.png" class="img-circle img-size-32 mr-2">
+                                                    Rifaldy Thoriq
+                                                </td>
+                                                <td>522 POIN</td>
+                                            </tr>
+                                            <tr>
+                                                <td>10</td>
+                                                <td>
+                                                    <img src="@/assets/img/default-150x150.png" class="img-circle img-size-32 mr-2">
+                                                    Muhammad Abdul Azis
+                                                </td>
+                                                <td>501 POIN</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <!-- /.container-fluid -->
+                </div>
+            <!-- /.content -->
+            </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+            <footer class="main-footer">
+                <strong>Copyright &copy; 2020 <a href="http://raharja.ac.id">Universitas Raharja</a>.</strong>
+                All rights reserved.
+                <div class="float-right d-none d-sm-inline-block">
+                <b>Version</b> 0.0.1
+                </div>
+            </footer>
+        </div>
+</template>
