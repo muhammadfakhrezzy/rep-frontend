db3826d0ffe1b5cbe3e29ff467362a70655914ab
hibiru-code <hibiru.code@gmail.com>
2020-03-16 15:50:39 +0700

    feat: add library font awesome arrowCircleRight

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import router from './router'
 import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFile, faTasks, faList, faAngleLeft, faSignOutAlt, faBars, faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faFile, faTasks, faList, faAngleLeft, faSignOutAlt, faBars, faCircle, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faHackerrank, faGoogle } from '@fortawesome/free-brands-svg-icons'
 import {faBell} from '@fortawesome/free-regular-svg-icons'
 
-library.add(faFile, faHackerrank, faTasks, faList, faAngleLeft, faBell, faSignOutAlt, faBars, faCircle, faGoogle)
+library.add(faFile, faHackerrank, faTasks, faList, faAngleLeft, faBell, faSignOutAlt, faBars, faCircle, faGoogle, faArrowCircleRight)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
