eb4e0e8c30ae4118f0cc5aa0270d520872316a02
hibiru-code <hibiru.code@gmail.com>
2020-03-30 06:27:42 +0700

    docs: changes to dinamic quest list

--- src/components/user/views/daily-quest/DailyList.vue
+++ src/components/user/views/daily-quest/DailyList.vue
@@ -1,3 +1,22 @@
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            daily: '',
+            quest_id : ''
+        }
+    },
+    async created() {
+        await axios.get('https://dytlan.alphabetincubator.id/api/superuser/difficulties/2')
+            .then(response => {
+                console.log(response)
+                this.daily = response.data[0].quests
+            })
+    }
+}
+</script>
+
 <template>
     <div class="content-wrapper">
         <div class="content-header">
@@ -12,109 +31,19 @@
         <div class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-3 col-6">
+                    <div class="col-lg-3 col-6" v-for="index in daily" :key="index.id">
                         <div class="small-box bg-olive">
                             <div class="inner">
                                 <h3>
-                                    1 POIN
-                                    <font-awesome-icon icon="info-circle" style="position: absolute; right: .5rem; cursor: pointer; font-size: 1.5rem" data-toggle="modal" data-target="#absensi"/>
+                                    {{ index.value }} POIN
+                                    <font-awesome-icon icon="info-circle" style="position: absolute; right: .5rem; cursor: pointer; font-size: 1.5rem" data-toggle="modal" :data-target="['#' + index.name + index.id]"/>
                                 </h3>
-                                <p>Absensi</p>
+                                <p>{{ index.name }}</p>
                             </div>
                             <div class="icon">
 
                             </div>
-                            <router-link to="/dailyAbsensi" class="small-box-footer">
-                                Start Quest
-                                <font-awesome-icon icon="arrow-circle-right" />
-                            </router-link>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-olive">
-                            <div class="inner">
-                                <h3>
-                                    1 POIN
-                                    <font-awesome-icon icon="info-circle" style="position: absolute; right: .5rem; cursor: pointer; font-size: 1.5rem" data-toggle="modal" data-target="#bbc"/>
-                                </h3>
-                                <p>BBC</p>
-                            </div>
-                            <div class="icon">
-
-                            </div>
-                            <router-link to="/dailybbc" class="small-box-footer">
-                                Start Quest
-                                <font-awesome-icon icon="arrow-circle-right" />
-                            </router-link>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-olive">
-                            <div class="inner">
-                                <h3>
-                                    1 POIN
-                                    <font-awesome-icon icon="info-circle" style="position: absolute; right: .5rem; cursor: pointer; font-size: 1.5rem" data-toggle="modal" data-target="#vlc"/>
-                                </h3>
-                                <p>VLC</p>
-                            </div>
-                            <div class="icon">
-
-                            </div>
-                            <router-link to="/dailyvlc" class="small-box-footer">
-                                Start Quest
-                                <font-awesome-icon icon="arrow-circle-right" />
-                            </router-link>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-olive">
-                            <div class="inner">
-                                <h3>
-                                    1 POIN
-                                    <font-awesome-icon icon="info-circle" style="position: absolute; right: .5rem; cursor: pointer; font-size: 1.5rem" data-toggle="modal" data-target="#share"/>
-                                </h3>
-                                <p>Share Social Media</p>
-                            </div>
-                            <div class="icon">
-
-                            </div>
-                            <router-link to="/dailyshare" class="small-box-footer">
-                                Start Quest
-                                <font-awesome-icon icon="arrow-circle-right" />
-                            </router-link>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-olive">
-                            <div class="inner">
-                                <h3>
-                                    2 POIN
-                                    <font-awesome-icon icon="info-circle" style="position: absolute; right: .5rem; cursor: pointer; font-size: 1.5rem" data-toggle="modal" data-target="#cermi"/>
-                                </h3>
-                                <p>Cermi</p>
-                            </div>
-                            <div class="icon">
-
-                            </div>
-                            <router-link to="/dailycermi" class="small-box-footer">
-                                Start Quest
-                                <font-awesome-icon icon="arrow-circle-right" />
-                            </router-link>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-olive">
-                            <div class="inner">
-                                <h3>
-                                    2 POIN
-                                    <font-awesome-icon icon="info-circle" style="position: absolute; right: .5rem; cursor: pointer; font-size: 1.5rem" data-toggle="modal" data-target="#ltai"/>
-                                </h3>
-                                <p>LTAI</p>
-                            </div>
-                            <div class="icon">
-
-                            </div>
-                            <router-link to="/dailyltai" class="small-box-footer">
+                            <router-link :to="{name : 'questDailyStart', params: { quest_name: index.name, quest_id: index.id }}" class="small-box-footer">
                                 Start Quest
                                 <font-awesome-icon icon="arrow-circle-right" />
                             </router-link>
@@ -124,98 +53,18 @@
             </div>
 
 
-            <div class="modal fade" id="absensi">
+            <div class="modal fade" :id="[index.name + index.id]" v-for="index in daily" :key="index.id">
                 <div class="modal-dialog" style="margin-top: 50vh; transform: translateY(-50%)">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h4 class="modal-title">Absensi</h4>
+                            <h4 class="modal-title">{{ index.name }}</h4>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <font-awesome-icon icon="times" aria-hidden="true" />
                             </button>
                         </div>
                         <div class="modal-body pt-0">
                             <h5>Quest Description</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal fade" id="bbc">
-                <div class="modal-dialog" style="margin-top: 50vh; transform: translateY(-50%)">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">BBC</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <font-awesome-icon icon="times" aria-hidden="true" />
-                            </button>
-                        </div>
-                        <div class="modal-body pt-0">
-                            <h5>Quest Description</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal fade" id="vlc">
-                <div class="modal-dialog" style="margin-top: 50vh; transform: translateY(-50%)">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">VLC</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <font-awesome-icon icon="times" aria-hidden="true" />
-                            </button>
-                        </div>
-                        <div class="modal-body pt-0">
-                            <h5>Quest Description</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal fade" id="share">
-                <div class="modal-dialog" style="margin-top: 50vh; transform: translateY(-50%)">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Share Media Social</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <font-awesome-icon icon="times" aria-hidden="true" />
-                            </button>
-                        </div>
-                        <div class="modal-body pt-0">
-                            <h5>Quest Description</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal fade" id="cermi">
-                <div class="modal-dialog" style="margin-top: 50vh; transform: translateY(-50%)">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">CerMi</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <font-awesome-icon icon="times" aria-hidden="true" />
-                            </button>
-                        </div>
-                        <div class="modal-body pt-0">
-                            <h5>Quest Description</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal fade" id="ltai">
-                <div class="modal-dialog" style="margin-top: 50vh; transform: translateY(-50%)">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">LTAI</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <font-awesome-icon icon="times" aria-hidden="true" />
-                            </button>
-                        </div>
-                        <div class="modal-body pt-0">
-                            <h5>Quest Description</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p>{{ index.description }}</p>
                         </div>
                     </div>
                 </div>
