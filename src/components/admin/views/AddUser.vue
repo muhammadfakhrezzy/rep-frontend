0bc3e90a20c025ab37b385a80d58e798f629a635
ikbal512 <iqbal@raharja.info>
2020-04-14 15:11:23 +0700

    add user

--- src/components/admin/views/AddUser.vue
+++ src/components/admin/views/AddUser.vue
@@ -5,11 +5,24 @@ import router from '../../../router'
 export default {
     data() {
         return {
+            user:[],
+            choose: '',
+            loading: '',
+            checkbox: '',
+            email:'',
             name:''
         }
     },
     methods: {
         submit() {
+            if(!this.choose || !this.checkbox) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!'
+                })
+                return
+            }
             Swal.fire({
                 title: 'Are you sure want to add this user ?',
                 icon: 'warning',
@@ -17,23 +30,37 @@ export default {
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Yes'
-            })  .then(result => {
-                    if(result.value) {
-                        axios.post('https://dev.alphabetincubator.id/rep-backend/public/api/superuser/users')
-                            .then(response => {
-                                this.name=response.data
-                                console.log(response)
-                                Swal.fire(
-                                    'Sucess!',
-                                    'User has been added',
-                                )
-                            })
-                            .catch(error => {
-                                console.log(error)
-                            })
-                    }
+            }).then(result => {
+                if(result.value) {
+                    axios.post('https://dev.alphabetincubator.id/rep-backend/public/api/superuser/users', + {name : this.choose},{email: this.choose},{user_id : this.choose})
+                        .then(response => {
+                            console.log(response)
+                            Swal.fire(
+                                'Success!',
+                                'User has been added.'
+                            )
+                        })
+                        .catch(error => console.log(error))
+                }
             })
         }
+    },
+    computed: {
+        load(){
+            return this.loading === 'loading'
+        }
+    },
+    created() {
+        this.loading = 'loading'
+        axios.get('https://dev.alphabetincubator.id/rep-backend/public/api/superuser/users/create')
+            .then(response => {
+                console.log(response)
+                for(let key in response.data[0]){
+                    const obj = response.data[0][key]
+                    this.user.push(obj)
+                    this.loading = ''
+                }
+            })
     }
 }
 </script>
@@ -63,17 +90,21 @@ export default {
                                         <div class="col-sm-12">
                                             <div class="form-group">
                                                 <label>Email</label>
-                                                <input type="email" class="form-control">
+                                                <input type="email" class="form-control" v-model="email">
                                             </div>
                                             <div class="form-group">
                                                 <label>Name</label>
-                                                <input type="email" class="form-control">
+                                                <input type="email" class="form-control" v-model="name">
                                             </div>
                                             <div class="form-group">
                                                 <label>Role</label>
                                                 <select class="form-control select2" style="width: 100%;">
-                                                    <option></option>
+                                                    <option v-for="(index) in user" :key="index.id" :value="index.id">{{ index.name }}</option>
                                                 </select>
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input type="checkbox" v-model="checkbox" class="form-check-input">
+                                                <label class="form-check-label">I agree to the <a href="#">terms of service</a>.</label>
                                             </div>
                                         </div>
                                     </div>
